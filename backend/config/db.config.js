ec78d053adf6ca844e4af715763398039e03117c
gbrlmarn <gbrlmarn@gmail.com>
2021-07-18 21:10:48 +0300

    reindentation

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 // local mysql db connection
 
 const dbConn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Parola',
-    database: 'node_mysql_crud_db'
+  host: "localhost",
+  user: "root",
+  password: "Parola",
+  database: "node_mysql_crud_db",
 });
 
-dbConn.connect(function(err) {
-    if (err) throw err;
-    console.log("Database Connected!");
+dbConn.connect(function (err) {
+  if (err) throw err;
+  console.log("Database Connected!");
 });
 
 module.exports = dbConn;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 // create express app
 const app = express();
@@ -8,23 +8,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // parse request of content-type - applicatoin/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // define a root route
-app.get('/', (req, res) => {
-    res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 // Require user routes
-const userRoutes = require('./src/routes/user.routes')
+const userRoutes = require("./src/routes/user.routes");
 
 // using as middleware
-app.use('/api/v1/users', userRoutes)
+app.use("/api/users", userRoutes);
 
 // listen for requests
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
